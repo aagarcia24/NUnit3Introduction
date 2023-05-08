c0d45bc2b2b405d78798f523e2952644cfac7fe5
Alvaro Garcia <aagarcia24@hotmail.com>
2023-05-08 14:25:46 -0500

    module 6

---
+++ Loans.Tests/MonthlyRepaymentCsvData.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+
+namespace Loans.Tests
+{
+    public class MonthlyRepaymentCsvData
+    {
+        public static IEnumerable GetTestCases(string csvFileName)
+        {
+            var csvLines = File.ReadAllLines(csvFileName);
+            var testCases = new List<TestCaseData>();
+
+            foreach (var line in csvLines)
+            {
+                string[] values = line.Replace(" ", "").Split(',');
+
+                decimal principal = decimal.Parse(values[0], NumberStyles.AllowDecimalPoint, CultureInfo.InvariantCulture);
+                decimal interestRate = decimal.Parse(values[1], NumberStyles.AllowDecimalPoint, CultureInfo.InvariantCulture);
+                int termInYears = int.Parse(values[2]);
+                decimal expectedRepayment = decimal.Parse(values[3], NumberStyles.AllowDecimalPoint, CultureInfo.InvariantCulture);
+
+                testCases.Add(new TestCaseData(principal, interestRate, termInYears, expectedRepayment));
+            }
+
+            return testCases;
+        }
+    }
+}
---
+++ Loans.Tests/ProductComparisonAttribute.cs
@@ -0,0 +1,10 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace Loans.Tests
+{
+    [AttributeUsage(AttributeTargets.Method | AttributeTargets.Class, AllowMultiple = false)]
+    public class ProductComparisonAttribute : CategoryAttribute
+    {
+    }
+}
